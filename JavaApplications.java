8f74fb401df8fb06f0feeca6e3dadab99b47b921
Kawnain <91244809+Kawnain@users.noreply.github.com>
2022-10-02 19:25:48 +0530

    Create JavaApplications.java

---
+++ JavaApplications.java
@@ -0,0 +1,7 @@
+����>< 
+ java/lang/Object<init>()V		 
+ java/lang/SystemoutLjava/io/PrintStream; enter the mark in subject A : B  
+ java/io/PrintStreamprintln(Ljava/lang/String;)Vjava/util/Scanner	inLjava/io/InputStream; 
+ (Ljava/io/InputStream;)V 
+ !nextInt()I#STUDENT QUALIFY FOR DEEGRY %!student reapear to qualify exam B'student is fail)#javaapplication10/JavaApplication10CodeLineNumberTableLocalVariableTablethis%Ljavaapplication10/JavaApplication10;main([Ljava/lang/String;)Vargs[Ljava/lang/String;AIBscLjava/util/Scanner; StackMapTableMethodParameters 
+ SourceFileJavaApplication10.java!(*/*��+,-.	/0*�k� ��Y��N-�<-�=7�-��"��9-�7��"��"A�-��$���&��+2 )4@KWbj,*k12S34N54X678 �491:;
