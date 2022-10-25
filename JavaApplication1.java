dd6660075608c28ec26b804adc17fc9cee795d0e
Kawnain <91244809+Kawnain@users.noreply.github.com>
2022-10-25 12:53:40 +0530

    Add files via upload

--- JavaApplication1.java
+++ JavaApplication1.java
@@ -1,20 +1,10 @@
-package javaapplication1; 
-  
- import java.util.Scanner; 
-  
- public class JavaApplication1 { 
-  
-     public static void main(String[] args) { 
-       int year; 
-       System.out.println("Enter an Year :: "); 
-       Scanner sc = new Scanner(System.in); 
-       year = sc.nextInt(); 
-  
-       if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)) 
-          System.out.println(" year is a leap year"); 
-       else 
-          System.out.println("year is not a leap year"); 
-  
-     } 
-      
- }
+
+package javaapplication1;
+public class JavaApplication1 {
+
+ 
+    public static void main(String[] args) {
+       
+    }
+    
+}
